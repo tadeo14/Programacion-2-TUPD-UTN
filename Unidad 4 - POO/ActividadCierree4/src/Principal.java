bc6d3bdb6067fe96d9d589ed11c3d2775730bd66
tadeo <tadeoacosta14@gmail.com>
2025-09-24 19:09:07 -0300

    aplico las actualizaciones

--- Unidad 4 - POO/ActividadCierree4/src/Principal.java
+++ Unidad 4 - POO/ActividadCierree4/src/Principal.java
@@ -16,7 +16,9 @@ public class Principal {
         Empleado e1 = new Empleado(10, "Ana García", "Analista", 55000.0);
         Empleado e2 = new Empleado(11, "Juan Pérez", "Desarrollador", 72000.0);
     
-    
+    // Instanciando usando constructor con nombre y puesto (id automático, salario por defecto)
+        Empleado e3 = new Empleado("María López", "Soporte");
+        Empleado e4 = new Empleado("Pedro Gómez", "RRHH");
     
     
     
