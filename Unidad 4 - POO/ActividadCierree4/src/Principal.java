39c30b7440c8659579c7b2f634e38d4a7f3919fd
tadeo <tadeoacosta14@gmail.com>
2025-09-24 19:10:48 -0300

    muestro la informacion de los empleados

--- Unidad 4 - POO/ActividadCierree4/src/Principal.java
+++ Unidad 4 - POO/ActividadCierree4/src/Principal.java
@@ -19,7 +19,21 @@ public class Principal {
     // Instanciando usando constructor con nombre y puesto (id automático, salario por defecto)
         Empleado e3 = new Empleado("María López", "Soporte");
         Empleado e4 = new Empleado("Pedro Gómez", "RRHH");
-    
+        
+        // Aplicando actualizaciones de salario
+        e1.actualizarSalario(10);      // +10% a Ana
+        e2.actualizarSalario(3000.0);  // +3000 fijos a Juan
+        e3.actualizarSalario(5);       // +5% a María (salario por defecto aumentado)
+        e4.actualizarSalario(1500.0);  // +1500 fijos a Pedro
+        
+        // Imprimir la información de cada empleado
+        System.out.println(e1.toString());
+        System.out.println(e2.toString());
+        System.out.println(e3.toString());
+        System.out.println(e4.toString());
+
+        // Mostrar total de empleados creados
+        System.out.println("Total empleados creados: " + Empleado.mostrarTotalEmpleados());
     
     
     
